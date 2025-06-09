2b38a37403285c8aab663e9843430a4002c93761
guitadharsh <adharshd100@gmail.com>
2025-06-09 23:27:34 +0530

    first commit

---
+++ app/routes/_index.tsx
@@ -0,0 +1,11 @@
+import Hero from "~/components/hero/Hero";
+import UnderConstruction from "~/components/under-construction/UnderConstruction";
+
+export default function Index() {
+  return (
+    <div className="scroll-smooth">
+      <Hero />
+      <UnderConstruction />
+    </div>
+  );
+}
